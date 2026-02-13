92350f3020ff4dbc7bfc26038dd67313e97c2d1a
Pratthika <119473299+Pratthika@users.noreply.github.com>
2026-02-13 10:43:35 +0530

    Add files via upload

---
+++ src/context/AuthContext.js
@@ -0,0 +1,28 @@
+// AuthContext.js
+import React, { createContext, useState, useContext } from 'react';
+
+const AuthContext = createContext();
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = () => {
+    // Simulate login by setting isLoggedIn to true
+    setIsLoggedIn(true);
+  };
+
+  const logout = () => {
+    // Simulate logout by setting isLoggedIn to false
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
